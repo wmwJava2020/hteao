efc75450a1d424c5017c8ab5f84e137a3b30208f
jack <wondafrashmw@gmail.com>
2026-07-01 13:06:26 -0500

    Revert " logger to controller class"
    This reverts commit 987c5d7e

--- src/main/java/com/xpo/HTEAO/controller/HteaoController.java
+++ src/main/java/com/xpo/HTEAO/controller/HteaoController.java
@@ -2,11 +2,10 @@ package com.xpo.HTEAO.controller;
 
 import com.xpo.HTEAO.order.Hteao;
 import com.xpo.HTEAO.service.HteaoImpl;
-import com.xpo.HTEAO.service.HteaoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/v1/tea")
@@ -19,11 +18,6 @@ public class HteaoController {
     public HteaoController(HteaoImpl hteaoImpl) {
         log.info("HteaoController initialized with HteaoImpl");
         this.hteaoImpl = hteaoImpl;
-    //private HteaoImpl hteaoImpl;
-    private HteaoService hteaoService;
-
-    public HteaoController(HteaoService hteaoService) {
-        this.hteaoService = hteaoService;
     }
 
     @PostMapping("/order")
@@ -32,7 +26,6 @@ public class HteaoController {
         log.info("Received order request for tea: {}", hteao);
         hteaoImpl.order(hteao);
         log.info("Order placed successfully for tea: {}", hteao);
-        hteaoService.order(hteao);
         return ResponseEntity.ok("Order placed successfully");
     }
 
@@ -47,25 +40,6 @@ public class HteaoController {
         }
         hteaoImpl.save(hteao);
         log.info("Saved tea successfully for tea: {}", hteao);
-            hteao.setHteaoId(hteao.getHteaoId() * 2L);
-            hteao.setQuantity(hteao.getQuantity() + 1);
-            hteao.setPrice(hteao.getPrice().multiply(BigDecimal.valueOf(hteao.getQuantity().doubleValue())));
-            hteaoService.save(hteao);
-        }
-        hteaoService.save(hteao);
         return ResponseEntity.ok("Tea saved successfully");
     }
-
-    @GetMapping("/quantity")
-    public BigDecimal getTotalPrice(@RequestParam(defaultValue = "0") Integer quantity,
-                                    @RequestParam(defaultValue = "0") BigDecimal price) {
-        return hteaoService.getTotalPrice(quantity, price);
-    }
-
-    @GetMapping("/search")
-    public Hteao findProduct(Long id) {
-
-        return hteaoService.getHteaoById(id);
-    }
 }
-
