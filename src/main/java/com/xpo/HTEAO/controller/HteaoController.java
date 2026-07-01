987c5d7e6fdb9addb11aafaaaad9123689a1c6d2
jack <wondafrashmw@gmail.com>
2026-07-01 12:46:43 -0500

    logger to controller class

--- src/main/java/com/xpo/HTEAO/controller/HteaoController.java
+++ src/main/java/com/xpo/HTEAO/controller/HteaoController.java
@@ -2,6 +2,8 @@ package com.xpo.HTEAO.controller;
 
 import com.xpo.HTEAO.order.Hteao;
 import com.xpo.HTEAO.service.HteaoImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,27 +11,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/tea")
 public class HteaoController {
 
+    Logger log  = LoggerFactory.getLogger(HteaoController.class);
+    Hteao hteao;
     private HteaoImpl hteaoImpl;
 
     public HteaoController(HteaoImpl hteaoImpl) {
+        log.info("HteaoController initialized with HteaoImpl");
         this.hteaoImpl = hteaoImpl;
     }
 
     @PostMapping("/order")
     public ResponseEntity<?> orderTea(@RequestBody Hteao hteao) {
         // Implementation of orderTea method
+        log.info("Received order request for tea: {}", hteao);
         hteaoImpl.order(hteao);
+        log.info("Order placed successfully for tea: {}", hteao);
         return ResponseEntity.ok("Order placed successfully");
     }
 
     @PostMapping("/save")
     public ResponseEntity<?> saveTea(@RequestBody Hteao hteao) {
+        log.info("Received save request for tea: {}", hteao);
         for(int i = 0; i < 5; i++) {
             hteao.setHteaoId(hteao.getHteaoId() + i);
             hteao.setPrice(hteao.getPrice() + i * 2.99);
             hteaoImpl.save(hteao);
+            log.info("Saved tea with ID: {} and Price: {}", hteao.getHteaoId(), hteao.getPrice());
         }
         hteaoImpl.save(hteao);
+        log.info("Saved tea successfully for tea: {}", hteao);
         return ResponseEntity.ok("Tea saved successfully");
     }
 }
